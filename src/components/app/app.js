6ad24d40fb3193769c8f260c470bd5c0315f5565
Alexandrbolshakov <alexandrbolshakov@yahoo.com>
2020-04-23 09:28:30 +0300

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+import './app.css';
+
+const App = () => {
+
+    const data = [
+        {label: "Going to learn react", important: true, id: 'btbt'},
+        {label: "That is so goog", important: false, id: 'elc,'},
+        {label: "I need a break...", important: false, id: 'vfbbtb'}
+    ];
+    return (
+        <div className = "app">
+                <AppHeader/>
+            <div className = "search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+                <PostList posts = {data}/>
+                <PostAddForm/>
+        </div>
+    )
+};
+
+export default App;
